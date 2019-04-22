000b3ca31c5ef13ed536b775084028d381bf1c80
wuzuchang <Wzc949588>
2019-04-22 22:57:55 +0800

    Initial commit

---
+++ src/com/wzc/ui/MainActivity.java
@@ -0,0 +1,32 @@
+package com.wzc.ui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainActivity {
+    private JPanel panel1;
+    private JTextField draftSize;
+    private JTextField textField2;
+    private JButton btAddSmallestWidth;
+    private JList list1;
+    private JButton btCreateDirectory;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainActivity");
+        frame.setContentPane(new MainActivity().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MainActivity() {
+        btCreateDirectory.addActionListener(e -> {
+
+        });
+        btAddSmallestWidth.addActionListener(e -> {
+
+        });
+    }
+
+}
