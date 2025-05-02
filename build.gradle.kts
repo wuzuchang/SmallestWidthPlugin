dd59db31fe91ac6a31038990315ed12e54c5c0e2
wuzuchang <wuzuchang1994@gmail.com>
2025-05-02 17:32:58 +0800

    兼容新版本idea

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,65 @@
+// 参考：https://github.com/JetBrains/intellij-platform-plugin-template/tree/main
 plugins {
   id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.8.22"
-  id("org.jetbrains.intellij") version "1.14.1"
+  id("org.jetbrains.kotlin.jvm") version "1.9.25"
+  id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
 group = "com.wzc.sw.plugin"
-version = "1.3.2"
+version = "1.3.3"
 
 repositories {
   mavenCentral()
+  intellijPlatform {
+    defaultRepositories()
+  }
 }
 
 // Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-  version.set("2022.2.5")
-  type.set("IC") // Target IDE Platform
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+  implementation("org.dom4j:dom4j:2.1.4")
+  intellijPlatform {
+    create("IC", "2024.2.5")
 
-  plugins.set(listOf())
+    // Add necessary plugin dependencies for compilation here, example:
+    // bundledPlugin("com.intellij.java")
+  }
+}
+
+intellijPlatform {
+  pluginConfiguration {
+    ideaVersion {
+      sinceBuild = providers.gradleProperty("pluginSinceBuild")
+      untilBuild = provider { null }
+    }
+  }
+
+  signing {
+    certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+    privateKey = providers.environmentVariable("PRIVATE_KEY")
+    password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+  }
+
+  publishing {
+    token = providers.environmentVariable("PUBLISH_TOKEN")
+   }
 }
 
 tasks {
   // Set the JVM compatibility versions
   withType<JavaCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
+    sourceCompatibility = "18"
+    targetCompatibility = "18"
+    // 解决编译时中文报错
+    options.encoding = "UTF-8"
   }
+  // 添加以下内容，解决运行时控制台中文乱码
+  withType<JavaExec> {
+    jvmArgs = listOf("-Dfile.encoding=UTF-8", "-Dsun.stdout.encoding=UTF-8", "-Dsun.stderr.encoding=UTF-8")
+  }
+
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = "18"
   }
-
-  patchPluginXml {
-    sinceBuild.set("173")
-    untilBuild.set("232.*")
-  }
-
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
-
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
-}
-
-dependencies{
-  implementation("org.dom4j:dom4j:2.1.3")
 }
